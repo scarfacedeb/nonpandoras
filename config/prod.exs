fb312f89ac9921328bb96f7755778bdf62a2b335
scarfacedeb <scarfacedeb@gmail.com>
2018-10-18 02:29:46 +0300

    Update config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -51,13 +51,7 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :nonpandoras, NonpandorasWeb.Endpoint, server: true
-#
+config :phoenix, :serve_endpoints, true
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
