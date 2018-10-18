419faddda5f47cd79fd92752ec4bc66720cee436
scarfacedeb <scarfacedeb@gmail.com>
2018-10-18 11:30:15 +0300

    Fix config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :nonpandoras, NonpandorasWeb.Endpoint,
   load_from_system_env: true,
-  url: [port: 7777],
+  http: [port: 7777],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
